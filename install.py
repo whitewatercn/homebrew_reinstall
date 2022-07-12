fc600353ca0965d091987fdd1c7fde328af0ee18
王白水 <whitewatercn@qq.com>
2022-07-12 22:42:16 +0800

    Create install.py

---
+++ install.py
@@ -0,0 +1,10 @@
+#BatchInstall.py
+import os
+homebrews = {"upgrade " /
+        "anki",}
+try:
+    for homebrew in wingets:
+        os.system("homebrew install"+homebrew)
+    print("Successful")        
+except:
+    print("Failed Somehow")
